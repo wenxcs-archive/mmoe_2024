a942b1e5858d5c828bac0fb94e1209e3f8aff4c6
Your Name <you@example.com>
2024-06-03 14:11:10 +0000

    1

--- test/mmoe/cuda_sm80/test_linear.py
+++ test/mmoe/cuda_sm80/test_linear.py
@@ -161,3 +161,79 @@ def test_performance_block2_expert2(m=12800, k=4096, n=1024, index_size=256):
     logger.info(f"Average time(ms):{avg_time}")
     logger.info(f"TFLOPS: {2 * index_size * k * n / (avg_time * 1e-3) / 1e12}")
     logger.info(f"Throughput(G/s): {(m*k + k*index_size + m * index_size)/ (avg_time * 1e-3) / 1e9}")
+
+
+def test_performance_block16_expert16(m=12800, k=4096, n=1024, expert_num=16):
+    torch.manual_seed(12345)
+    index_size=expert_num*128
+    # inputs
+    w = torch.randn(expert_num, k, m).half().cuda()
+    act = torch.randn(index_size, k).half().cuda()
+    outp = torch.zeros(index_size, m).half().cuda()
+    # meta
+    index = torch.arange(0, index_size, dtype=torch.int32).cuda()
+    w_scale = torch.ones(expert_num).half().cuda()
+    expert_ids = torch.arange(0, expert_num, dtype=torch.int32).cuda()
+    num_tokens_post_padded = torch.tensor(index_size, dtype=torch.int32).cuda()
+    num_valid_tokens = index_size
+    topk = 2
+    topk_weights = torch.ones(n, topk).half().cuda()
+
+    mmoe.moe.cuda_sm80.moe_linear_Wf8_Af16_Of16_Accf32.moe_linear(
+        m, k, n, expert_num, index_size,
+        w,
+        act,
+        outp,
+        index,
+        w_scale,
+        topk_weights,
+        expert_ids,
+        num_tokens_post_padded,
+        num_valid_tokens,
+        topk,
+        1
+    )
+
+    outps = torch.chunk(outp, expert_num, dim=0)
+
+    for i in range(expert_num):
+        w_ = w[i, :, :]
+        a_ = act[i*128:(i+1)*128, :]
+        c_ = a_ @ w_
+        torch.testing.assert_close(outps[i], c_)
+
+    times = 100
+    all_time = 0.0
+    for j in range(10 + times):
+        start = torch.cuda.Event(enable_timing=True)
+        end = torch.cuda.Event(enable_timing=True)
+        start.record()
+    
+        mmoe.moe.cuda_sm80.moe_linear_Wf8_Af16_Of16_Accf32.moe_linear(
+            m, k, n, expert_num, index_size,
+            w,
+            act,
+            outp,
+            index,
+            w_scale,
+            topk_weights,
+            expert_ids,
+            num_tokens_post_padded,
+            num_valid_tokens,
+            topk,
+            1
+        )
+
+        end.record()
+        torch.cuda.synchronize()
+        elapsed_time_ms = start.elapsed_time(end)
+
+        if j >= 10:
+            all_time += elapsed_time_ms
+        
+    avg_time = all_time / times
+
+    logger.info(f"m={m}, k={k}, n={n}, index_size={index_size}, expert_num={expert_num}")
+    logger.info(f"Average time(ms):{avg_time}")
+    logger.info(f"TFLOPS: {2 * index_size * k * n / (avg_time * 1e-3) / 1e12}")
+    logger.info(f"Throughput(G/s): {(m*k + k*index_size + m * index_size)/ (avg_time * 1e-3) / 1e9}")
